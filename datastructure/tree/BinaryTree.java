1404b4983211f0651225ce677eb7eaa676fb3431
LucasMMBB <maoxummbb@gmail.com>
2018-09-25 20:21:20 -0400

    update BinaryTree methods new method: childrenNum

--- datastructure/tree/BinaryTree.java
+++ datastructure/tree/BinaryTree.java
@@ -16,6 +16,18 @@ public class BinaryTree {
         return maxDepthHelper(node);
     }
 
+    public int childrenNum() {
+        TreeNode node = root;
+        return childrenNumHelper(node);
+    }
+
+    private int childrenNumHelper(TreeNode node) {
+        if (node == null) { return 0; }
+        int left = childrenNumHelper(node.left);
+        int right = childrenNumHelper(node.right);
+        return left + right + 1;
+    }
+
     private int maxDepthHelper(TreeNode node) {
         if(node == null) { return 0; }
         return Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right)) + 1;
