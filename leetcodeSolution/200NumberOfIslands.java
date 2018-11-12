d59b8208d3e1797096fa0f1f51900ecd9f345ab1
LucasMMBB <maoxummbb@gmail.com>
2018-11-11 23:16:05 -0500

    add union find solution for Number of Islands

--- leetcodeSolution/200NumberOfIslands.java
+++ leetcodeSolution/200NumberOfIslands.java
@@ -37,6 +37,80 @@ public class Solution {
 	}
 }
 
-class Solution2 {
-	
+class Solution {
+	final int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+	/**
+	 * defind union find class
+	 */
+	public class UnionFind {
+		private int[] parent = null;
+		private int count;
+
+		private int find(int x) {
+			if (parent[x] == x) {
+				return x;
+			}
+			parent[x] = find(parent[x]);
+			return parent[x];
+		}
+		public UnionFind(int n) {
+            parent = new int[n];
+			for (int i = 0; i < n; i++) {
+				parent[i] = i;
+			}
+		}
+
+		public void connect(int a, int b){
+			int rootA = find(a), rootB = find(b);
+			if (rootA != rootB) {
+				parent[rootA] = rootB;
+				count--;
+			}
+		}
+		public int query() {
+			return count;
+		}
+		public void setCount(int total) {
+			count = total;
+		}
+	}
+	public int numIslands(char[][] grid) {
+		// sanity check
+		if (grid == null || grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+
+		final int rows = grid.length;
+		final int cols = grid[0].length;
+
+		UnionFind uf = new UnionFind(rows * cols);
+
+		int total = 0;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (grid[i][j] == '1') {
+					total++;
+				}
+			}
+		}
+		uf.setCount(total);
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (grid[i][j] == '1') {
+					grid[i][j] = '0';
+					for (int[] dir : dirs) {
+						int neiX = i + dir[0];
+						int neiY = j + dir[1];
+
+						if (neiX >= 0 && neiY >= 0 && neiX < rows && neiY < cols && grid[neiX][neiY] == '1' ) {
+                            uf.connect(i * cols + j, neiX * cols + neiY);
+                        }
+					}
+				}
+			}
+		}
+		return uf.query();
+
+	}
 }
