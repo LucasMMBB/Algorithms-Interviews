a49dc4b49988cbf27ff932507854426b1c701855
LucasMMBB <maoxummbb@gmail.com>
2018-11-15 21:39:47 -0500

    clean up

--- leetcodeSolution/796roateString.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public boolean rotateString(String A, String B) {
-        return (A.length() == B.length())
-        	&& (new StringBuilder(A).append(A).toString().contains(B));
-    }
-}
